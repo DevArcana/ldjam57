c176e58bfbb421e1fa77be8b50fbd57dfb0f4ede
Jadw1 <jadw1_1@o2.pl>
2025-04-06 15:40:11 +0200

    sonar - draw map

--- submarine/sonar/depths_map.gd
+++ submarine/sonar/depths_map.gd
@@ -1,2 +1,28 @@
 extends Node
 class_name DepthsMap
+
+@export var submarine_speed: float = 50.0
+
+class DepthObject:
+	var position: Vector2
+	var shape: PackedVector2Array
+
+@onready var submarine_position: Vector2 = Vector2.ZERO
+@onready var objects: Array[DepthObject] = []
+
+func test_object() -> DepthObject:
+	var object: DepthObject = DepthObject.new()
+	object.position = Vector2(50, 50)
+	object.shape = PackedVector2Array(Array([
+		Vector2(0.0, 0.0), Vector2(50.0, 10.0), Vector2(40.0, 60.0), Vector2(-10.0, 40.0)
+	]))
+	return object
+
+func _ready() -> void:
+	objects.append(test_object())
+	
+func _process(_delta: float) -> void:
+	if Input.is_action_pressed("submarine_forward"):
+		submarine_position += Vector2(0.0, submarine_speed * _delta)
+	elif Input.is_action_just_pressed("submarine_backward"):
+		submarine_position -= Vector2(0.0, submarine_speed * _delta * 0.5)
--- submarine/sonar/display.gd
+++ submarine/sonar/display.gd
@@ -9,10 +9,17 @@ func _ready() -> void:
 	print("Screen size: " + str(screen_size))
 
 func draw_submarine() -> void:
-	var pos: Vector2 = screen_size.get_center()
-	draw_circle(pos, 30, Color.DARK_GREEN)
+	draw_circle(screen_size.get_center(), 30, Color.DARK_GREEN)
+
+func draw_objects() -> void:
+	var center: Vector2 = screen_size.get_center()
+	for obj in depths_map.objects:
+		var transform: Transform2D = Transform2D(0.0, -(center + obj.position - depths_map.submarine_position))
+		var polygon: PackedVector2Array = obj.shape * transform
+		draw_colored_polygon(polygon, Color.LIGHT_GREEN)
 
 func _draw() -> void:
+	draw_objects()
 	draw_submarine()
 
 func _process(_delta: float) -> void:
